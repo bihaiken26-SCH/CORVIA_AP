d59a392029c2792bae55a96c18e9db0cfcdbd1e5
bihaiken26-SCH <bihaiken26@gmail.com>
2026-09-14 10:25:44 -0400

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+name: corvia
+description: "CORVIA - Passerelle Financière & Mobile Money (Moncash, Natcash, Unibank vers Binance, Wise, Meru)"
+publish_to: 'none'
+version: 1.0.0+1
+
+environment:
+  sdk: '>=3.0.0 <4.0.0'
+
+dependencies:
+  flutter:
+    sdk: flutter
+  # Icônes modernes
+  cupertino_icons: ^1.0.6
+  # Gestion des polices Google
+  google_fonts: ^6.1.0
+  # Formatage de devises et dates
+  intl: ^0.19.0
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^3.0.0
+
+flutter:
+  uses-material-design: true
+
